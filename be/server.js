a6df2199b3631b3991d2ee6b91a544e941b4edc4
f <44983936+codemagiciangamer@users.noreply.github.com>
2024-11-17 20:10:27 +0100

    v0.0.2

--- be/server.js
+++ be/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require("path")
 const app = express();
 const port = 9999;
 
@@ -9,9 +10,16 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
 });
+
+app.use(express.static(path.resolve(__dirname, "dist")))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"))
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
