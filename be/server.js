9a7683525a4f67c3acea8bbe862fa52bf4dc35ae
f <44983936+codemagiciangamer@users.noreply.github.com>
2024-11-17 19:51:35 +0100

    v0.0.1

---
+++ be/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 9999;
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-access-token");
+    next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
